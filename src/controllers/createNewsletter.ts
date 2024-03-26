13624da4ade922bbd688f05165cd79c4a5939d26
kostya <kostikmail.96@mail.ru>
2024-03-26 23:33:20 +0300

    update createNewsletter

--- src/controllers/createNewsletter.ts
+++ src/controllers/createNewsletter.ts
@@ -8,62 +8,77 @@ const sendAllMessage = async (dataFromFront: {
     title: string;
     text: string;
     threads: number;
+    paymentStatus: number;
 }) => {
     let clientList: INaumovaTeamClient[] = [];
     await getAllClientsFromDB().then((res) => {
         clientList = [].concat(JSON.parse(JSON.stringify(res)));
     });
 
-    const testArr = [
-        {
-            id: 1,
-            name: "Pabloerroeer",
-            email: "kostikmaппудil.96@mail.ru",
-            stream: 1,
-        },
+    // const testArr = [
+    //     {
+    //         id: 1,
+    //         name: "Pabloerroeer",
+    //         email: "kostikmaппудil.96@mail.ru",
+    //         stream: 1,
+    //         paymentStatus: 0,
+    //     },
 
-        { id: 5, name: "Pablo", email: "kostikmail.96@mail.ru", stream: 3 },
-        {
-            id: 2,
-            name: "Kostya",
-            email: "kypreichicsimba@gmail.com",
-            stream: 2,
-        },
-        { id: 3, name: "Pleo", email: "kostikmail.96@mail.ru", stream: 1 },
-        {
-            id: 4,
-            name: "Konstantsin",
-            email: "kypreichicsimba@gmail.com",
-            stream: 3,
-        },
+    //     { id: 5, name: "Pablo", email: "kostikmail.96@mail.ru", stream: 3 },
+    //     {
+    //         id: 2,
+    //         name: "Kostya",
+    //         email: "kypreichicsimba@gmail.com",
+    //         stream: 2,
+    //         paymentStatus: 1,
+    //     },
+    //     { id: 3, name: "Pleo", email: "kostikmail.96@mail.ru", stream: 1 },
+    //     {
+    //         id: 4,
+    //         name: "Konstantsin",
+    //         email: "kypreichicsimba@gmail.com",
+    //         stream: 3,
+    //         paymentStatus: 0,
+    //     },
 
-        {
-            id: 4,
-            name: "Kirill",
-            email: "kypreichicsimba@gmail.com",
-            stream: 3,
-        },
-        {
-            id: 6,
-            name: "Мikola",
-            email: "kypreichicsimba@gmail.com",
-            stream: 2,
-        },
-    ];
-
-    console.log("clientListLength: ", clientList.length);
+    //     {
+    //         id: 4,
+    //         name: "Kirill",
+    //         email: "kypreichicsimba@gmail.com",
+    //         stream: 3,
+    //         paymentStatus: 1,
+    //     },
+    //     {
+    //         id: 6,
+    //         name: "Мikola",
+    //         email: "kypreichicsimba@gmail.com",
+    //         stream: 2,
+    //         paymentStatus: 1,
+    //     },
+    // ];
 
     if (dataFromFront.threads === -1) {
         //* уникальные пользователи
-        const res = clientList.filter((el, i, array) => {
-            return (
-                array.findIndex(
-                    (o) => o.email.toLowerCase() === el.email.toLowerCase(),
-                ) === i
-            );
-        });
+        const res = clientList
+            .filter((el) => {
+                if (
+                    dataFromFront.paymentStatus !== 0 &&
+                    dataFromFront.paymentStatus !== 1
+                ) {
+                    return el;
+                } else {
+                    return el.paymentStatus === dataFromFront.paymentStatus;
+                }
+            })
+            .filter((el, i, array) => {
+                return (
+                    array.findIndex(
+                        (o) => o.email.toLowerCase() === el.email.toLowerCase(),
+                    ) === i
+                );
+            });
 
-        console.log("resLength", res.length);
+        console.log("res", res.length);
 
         for (let i = 0; i < res.length; i++) {
             const user = { ...res[i], ...dataFromFront };
@@ -74,9 +89,18 @@ const sendAllMessage = async (dataFromFront: {
             }
         }
     } else {
-        const res = clientList.filter(
-            (el) => el.stream === dataFromFront.threads,
-        );
+        const res = clientList
+            .filter((el) => {
+                if (
+                    dataFromFront.paymentStatus !== 0 &&
+                    dataFromFront.paymentStatus !== 1
+                ) {
+                    return el;
+                } else {
+                    return el.paymentStatus === dataFromFront.paymentStatus;
+                }
+            })
+            .filter((el) => el.stream === dataFromFront.threads);
 
         for (let i = 0; i < res.length; i++) {
             const user = { ...res[i], ...dataFromFront };
@@ -91,10 +115,12 @@ const sendAllMessage = async (dataFromFront: {
     }
 };
 
+// sendAllMessage({ paymentStatus: 1, text: "rr", threads: 14, title: "hello" });
+
 export const createNewsletter = async (req: Request, res: Response) => {
     try {
         if (req.query.apikey === process.env.LOGIN_KEY) {
-            const { title, text, threads } = req.body;
+            const { title, text, threads, paymentStatus } = req.body;
 
             if (!text || !title || !threads) {
                 return res.status(400).json({
@@ -104,7 +130,7 @@ export const createNewsletter = async (req: Request, res: Response) => {
                 });
             }
 
-            const dataFromFront = { text, title, threads };
+            const dataFromFront = { text, title, threads, paymentStatus };
             await sendAllMessage(dataFromFront);
 
             res.status(200).json({
